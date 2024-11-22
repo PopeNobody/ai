2d75e5f2f15b380afe81fe6b99c884b98c932915
nobody <github2@invisiblehand.church>
2024-11-22 06:52:58 -0500

    This version of the docs looks much better.

--- doc/gemini/suck-it.bash
+++ doc/gemini/suck-it.bash
@@ -4,8 +4,8 @@ mkdir -p gemini-api-docs
 # Download the English version of the main documentation page, preserving MIME
 # types, adjusting extensions, and removing special characters from filenames
 
-wget -r -np -nH --cut-dirs=3 -E -P gemini-api-docs 
---restrict-file-names=windows 
+wget -r -np -nH --cut-dirs=3 -E -P gemini-api-docs  \
+--restrict-file-names=windows  \
 https://ai.google.dev/gemini-api/docs/
 
 # Download the English version of the API reference
