40986f45f41923892fda2ebd96bd00dc0cfda12b
nobody <github2@invisiblehand.church>
2024-11-24 21:34:32 -0500

    x

--- doc/gemini/suck-it.bash
+++ doc/gemini/suck-it.bash
@@ -4,15 +4,19 @@ mkdir -p gemini-api-docs
 # Download the English version of the main documentation page, preserving MIME
 # types, adjusting extensions, and removing special characters from filenames
 
-wget -r -np -nH --cut-dirs=3 -E -P gemini-api-docs  \
+wget -r -np -nH -c --cut-dirs=3 -E -P gemini-api-docs  \
 --restrict-file-names=windows  \
 https://ai.google.dev/gemini-api/docs/
 
 # Download the English version of the API reference
-#wget -r -np -nH --cut-dirs=3 -E -P gemini-api-docs --restrict-file-names=windows https://ai.google.dev/gemini-api/reference/en/
+wget -r -np -nH --c -cut-dirs=3 -E -P gemini-api-docs \
+  --restrict-file-names=windows \
+  https://ai.google.dev/gemini-api/reference/en/
 
 # Download the English version of the cookbook
-#wget -r -np -nH --cut-dirs=3 -E -P gemini-api-docs --restrict-file-names=windows https://ai.google.dev/gemini-api/cookbook/en/
+wget -r -np -nH -c --cut-dirs=3 -E -P gemini-api-docs \
+  --restrict-file-names=windows \
+  https://ai.google.dev/gemini-api/cookbook/en/
 
 
 
